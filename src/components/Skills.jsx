652522140b3fdab58e23fe3b4da816b879b3e6a3
adminnono <arnaud.dujardin76@gmail.com>
2024-12-05 11:26:14 +0100

    ajout icone compétence

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,29 +1,20 @@
 import { useTrail, animated } from "react-spring";
-import {
-  FaHtml5,
-  FaCss3Alt,
-  FaReact,
-} from "react-icons/fa";
-import {
-  SiVisualstudiocode,
-  SiJavascript,
+import { FaHtml5, FaCss3Alt, FaReact, FaNodeJs } from "react-icons/fa"; // Icônes FontAwesome
+import { SiJavascript, SiVisualstudiocode, SiWordpress } from "react-icons/si"; // Icônes Simple Icons
+import { DiGithubBadge } from "react-icons/di"; // Icône GitHub Badge
 
-} from "react-icons/si";
-import { DiGithubBadge } from "react-icons/di";
-import { SiWordpress } from 'react-icons/si'; 
-import { SiPhp } from 'react-icons/si'; // Import de l'icône PHP
-
-
+// Liste des compétences avec leurs icônes
 const skills = [
   { id: "html", Component: FaHtml5 },
   { id: "css", Component: FaCss3Alt },
   { id: "javascript", Component: SiJavascript },
   { id: "react", Component: FaReact },
-  { id: "php", Component: SiPhp }, // Ajout de PHP
+  { id: "nodejs", Component: FaNodeJs }, // Ajout de Node.js
   { id: "vscode", Component: SiVisualstudiocode },
   { id: "github", Component: DiGithubBadge },
-  { id: "wordpress", Component: SiWordpress }, 
+  { id: "wordpress", Component: SiWordpress }
 ];
+
 
 export default function Skills() {
   const trail = useTrail(skills.length, {
@@ -37,7 +28,7 @@ export default function Skills() {
       className="bg-lightDesert rounded-lg shadow-md flex items-center justify-center min-h-screen"
     >
       <div className="text-center max-w-2xl w-full mx-auto">
-        <h2 className="text-4xl font-bold text-darkDesert mb-6">Compétences</h2>
+        <h2 className="text-4xl font-bold text-darkDesert mb-6">Compétences actuelles</h2>
         <div className="grid grid-cols-4 gap-4 my-4 justify-items-center">
           {trail.map((props, index) => {
             const { Component } = skills[index];

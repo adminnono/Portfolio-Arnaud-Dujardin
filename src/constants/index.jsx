a59f299e9f1ffc31c2125d36369a5e228d415c39
adminnono <arnaud.dujardin76@gmail.com>
2025-01-16 13:45:55 +0100

    en plus cv

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -3,6 +3,6 @@ export const LINKEDIN_URL = "https://www.linkedin.com/in/arnaud-dujardin-9044802
 export const PERSONAL_WEBSITE_URL = "";
 export const CURRENT_YEAR = new Date().getFullYear().toString();
 export const YOUTUBE_URL = "";
-export const RESUME_URL = "Curriculum.jpg";
+export const RESUME_URL = "Curriculum (2).pdf";
 export const BLOG_URL = "";
 export const PERPLEXITY_URL ="";

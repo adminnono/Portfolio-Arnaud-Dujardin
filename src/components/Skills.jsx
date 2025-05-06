5af7bcb82256cf0f272d45fb575cd364e763ed57
arnaud <arnaud.dujardin76@gmail.com>
2025-05-06 20:31:58 +0200

    Structuration

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -11,29 +11,42 @@ import { DiGithubBadge } from "react-icons/di";
 import { SiNotion } from "react-icons/si";
 import { SiMysql } from "react-icons/si";
 import { SiGit } from "react-icons/si";
-import { SiBootstrap, SiSass } from "react-icons/si";
+import { SiBootstrap, SiSass, SiVite, SiJquery, SiGutenberg } from "react-icons/si";
+import { SiWoocommerce, SiElementor } from "react-icons/si";
+
 
 
 
 // Liste des compétences avec leurs icônes
 const skills = [
+  { id: "wordpress", Component: SiWordpress, link: "/Wordpress.pdf" },
+  { id: "elementor", Component: SiElementor, link: "/elementor.pdf" },
+  { id: "gutenberg", Component: SiGutenberg, link: "/gutenberg.pdf" },
+  { id: "woocommerce", Component: SiWoocommerce, link: "/woocommerce.pdf" },
+  { id: "vscode", Component: SiVisualstudiocode, link: "/vsc.pdf" },
   { id: "html", Component: FaHtml5, link: "/html&css.pdf" },
   { id: "css", Component: FaCss3Alt, link: "/html&css.pdf" },
-  { id: "javascript", Component: SiJavascript, link: "/javascript.pdf" },
-  { id: "react", Component: FaReact, link: "/react.pdf" },
-  { id: "wordpress", Component: SiWordpress, link: "/Wordpress.pdf" },
+  { id: "sass", Component: SiSass, link: "/sass.pdf" },
+  { id: "bootstrap", Component: SiBootstrap, link: "/bootstrap.pdf" },
   { id: "php", Component: SiPhp, color: "#8993BE", link: "/Php.pdf" },
   { id: "mysql", Component: SiMysql, link: "/Php.pdf" },
-  { id: "vscode", Component: SiVisualstudiocode, link: "/vsc.pdf" },
+  { id: "javascript", Component: SiJavascript, link: "/javascript.pdf" },
+  { id: "jquery", Component: SiJquery, link: "/jquery.pdf" },
+  { id: "react", Component: FaReact, link: "/react.pdf" },
+  { id: "vite", Component: SiVite, link: "/vite.pdf" },
   { id: "github", Component: DiGithubBadge, link: "/github.pdf" },
   { id: "git", Component: SiGit, link: "/github.pdf" },
   { id: "figma", Component: SiFigma, link: "/figma.pdf" },
   { id: "notion", Component: SiNotion, link: "/notion.pdf" },
-  { id: "bootstrap", Component: SiBootstrap, link: "/bootstrap.pdf" },
-  { id: "sass", Component: SiSass, link: "/sass.pdf" },
 
 ];
 
+
+{skills.map(({ id, Component, link }) => (
+  <a key={id} href={link} className="btn btn-outline-primary m-2" target="_blank" rel="noopener noreferrer">
+    <Component size={id === "woocommerce" ? 72 : 48} />
+  </a>
+))}
 
 
 export default function Skills() {
@@ -51,19 +64,22 @@ export default function Skills() {
         <h2 className="text-4xl font-bold text-darkDesert mb-6">Mes compétences</h2>
         <div className="grid grid-cols-4 gap-4 my-4 justify-items-center">
           {trail.map((props, index) => {
-            const { Component, link } = skills[index];
+            const { Component, link, id } = skills[index];
+            
+            const iconSize = id === "woocommerce" ? 72 : 48;
+
             return (
               <animated.div
                 style={props}
-                className="text-5xl text-goldDesert transition-all duration-300 transform hover:scale-110 hover:text-darkDesert"
+                className="text-goldDesert transition-all duration-300 transform hover:scale-110 hover:text-darkDesert"
                 key={skills[index].id}
               >
                 {link ? (
                   <a href={link} target="_blank" rel="noopener noreferrer">
-                    <Component />
+                    <Component size={iconSize} />
                   </a>
                 ) : (
-                  <Component />
+                  <Component size={iconSize} />
                 )}
               </animated.div>
             );
